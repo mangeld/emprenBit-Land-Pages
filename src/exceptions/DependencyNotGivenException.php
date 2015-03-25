b3c835fba65eb148434b45681291a2e13a49252a
Miguel Ángel Durán González <contact@mangel.me>
2015-03-25 13:15:02 +0100

    Added exceptions

---
+++ src/exceptions/DependencyNotGivenException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace mangeld\exceptions;
+
+class DependencyNotGivenException extends \Exception { }
---
+++ src/exceptions/MalformatedStringException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace mangeld\exceptions;
+
+class MalformatedStringException extends \Exception { }
