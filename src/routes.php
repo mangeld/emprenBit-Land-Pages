933a4455ef6a1ef7c8c56e1f6b47828a159e176c
Miguel Ángel Durán González <contact@mangel.me>
2015-04-01 11:02:24 +0200

    Refactored code and added simple docs

--- src/routes.php
+++ src/routes.php
@@ -19,13 +19,6 @@ $slimApp->get('/logout', function(){
   echo 'logout';
 });
 
-$slimApp->get('/time', function() use ($slimApp){
-  $time = microtime(true);
-  $slimApp->response->setBody(
-    gettype($time).' - '.
-    $time);
-});
-
 $slimApp->get('/passHash/:pasw', function($pasw) use ($slimApp){
   $hasher = new \mangeld\lib\Hasher();
   $hash = $hasher->create_hash_blowfish($pasw, 12);
@@ -35,21 +28,44 @@ $slimApp->get('/passHash/:pasw', function($pasw) use ($slimApp){
 
 $slimApp->group('/v1', function() use ($slimApp, $app){
 
-  $slimApp->get('/pages/:id', function($id){
-    //TODO: Fetch data & define json object
+  /**
+   * Here we define the routes for the 'pages' resource, wich lets
+   * us list all the pages, add, remove, or get an specific Page data.
+   */
+  $slimApp->group('/pages', function() use ($slimApp, $app){
+
+    /**
+     * If you call the /v1/pages you get all the pages in the system
+     *
+     * TODO: Limit the data in each page & make pagination so the
+     * response isn't too big.
+     */
+    $slimApp->get('/', function() use ($slimApp, $app){
+      $pages = $app->getPages();
+      $slimApp->response->headers->set('Content-Type', 'application/json');
+      $slimApp->response->setBody($pages);
+    });
+
+    /**
+     * Add a page by it's name.
+     *
+     * Returns: A json object with the page created.
+     */
+    $slimApp->post('/:name', function($name){
+      //TODO: Add page & define json object
+      //TODO: Authenticate user
+    });
+
+    /**
+     * Delete a page from the system.
+     *
+     * Return: A json object with the status.
+     */
+    $slimApp->delete('/:id', function($id){
+      //TODO: Remove from database & send confirmation
+    });
+
   });
-
-  $slimApp->get('/pages', function() use ($slimApp, $app){
-    //TODO: Add page & define json object
-    //TODO: Authenticate user
-    
-    $pages = $app->getPages();
-    $slimApp->response->headers->set('Content-Type', 'application/json');
-    $slimApp->response->setBody($pages);
-  });
-
-
-
 });
 
 $slimApp->run();
