24f267f0007fb23d8b1858841b95db6ba60edf83
Miguel Ángel Durán González <contact@mangel.me>
2015-04-13 12:43:00 +0200

    Added JSON Response builder & associated tests.

---
+++ src/obj/JsonResponse.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace mangeld\obj;
+
+class JsonResponse
+{
+  private $meta;
+  private $data;
+
+  public function __construct()
+  {
+    $this->meta = new \StdClass();
+    $this->meta->code = 200;
+    $this->meta->errorType = '';
+    $this->meta->errorMessage = '';
+
+    $this->data = new \StdClass();
+  }
+
+  public static function badRequestFactory()
+  {
+    $jsonObj = new JsonResponse();
+    $jsonObj->meta->code = 400;
+    $jsonObj->meta->errorType = 'Invalid request';
+
+    return $jsonObj;
+  }
+
+  public static function resourceCreatedFactory()
+  {
+    $jsonObj = new JsonResponse();
+    $jsonObj->meta->code = 201;
+
+    return $jsonObj;
+  }
+
+  public function getMeta()
+  {
+    return $this->meta;
+  }
+
+  public function getData()
+  {
+    return $this->data;
+  }
+
+  public function setData($data)
+  {
+    $this->data = $data;
+  }
+
+  public function serialize()
+  {
+    return json_encode(get_object_vars($this));
+  }
+}
---
+++ tests/obj/JsonResponseTest.php
@@ -0,0 +1,73 @@
+<?php
+
+class JsonResponseTest extends PHPUnit_Framework_TestCase
+{
+  public function setUp()
+  {
+    $this->jsonResponse = new \mangeld\obj\JsonResponse();
+  }
+
+  public function tearDown()
+  {
+
+  }
+
+  private function validJsonObjSchema($code)
+  {
+    $meta = $this->jsonResponse->getMeta();
+
+    $this->assertInstanceOf('\mangeld\obj\JsonResponse', $this->jsonResponse);
+    $this->assertInstanceOf('\StdClass', $meta);
+    $this->assertInstanceOf('\StdClass', $this->jsonResponse->getData());
+    $this->assertInternalType('int', $meta->code, 'Status code is not an int');
+    $this->assertEquals($code, $meta->code);
+    $this->assertInternalType('string', $meta->errorType, 'Error type is not an string');
+    $this->assertInternalType('string', $meta->errorMessage, 'Error message is not an string');
+  }
+
+  public function testBasicResponseSchemaIsCreated()
+  {
+    $this->validJsonObjSchema(200);
+  }
+
+  public function testSetData()
+  {
+    $this->jsonResponse->setData(new \StdClass());
+    $serialized = $this->jsonResponse->serialize();
+    $jsonObj = json_decode($serialized);
+
+    $this->assertInstanceOf('\StdClass', $jsonObj);
+  }
+
+  public function testBasicSchemaIsSerialized()
+  {
+    $json = $this->jsonResponse->serialize();
+    $deserialized = json_decode($json);
+
+    $this->assertInternalType('int', $deserialized->meta->code, var_export($deserialized, true));
+    $this->assertInternalType('string', $deserialized->meta->errorType, var_export($deserialized, true));
+    $this->assertInternalType('string', $deserialized->meta->errorMessage, var_export($deserialized, true));
+    $this->assertInstanceOf('\StdClass', $deserialized->data, var_export($deserialized, true));
+  }
+
+  public function testBadResponseJsonSchemaIsCreated()
+  {
+    $this->jsonResponse = \mangeld\obj\JsonResponse::badRequestFactory();
+
+    $this->validJsonObjSchema(400);
+  }
+
+  public function testResourceCreatedJsonSchemaIsCreated()
+  {
+    $this->jsonResponse = \mangeld\obj\JsonResponse::resourceCreatedFactory();
+
+    $this->validJsonObjSchema(201);
+  }
+
+  public function testStringIsReturnedOnSerialize()
+  {
+    $json = $this->jsonResponse->serialize();
+
+    $this->assertInternalType('string', $json, 'Serialized json is not a string');
+  }
+}
