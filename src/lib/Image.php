fafa3b0cf1695f602930730ea4e42ce5c2662052
Miguel Ángel Durán González <contact@mangel.me>
2015-04-28 22:59:53 +0200

    Added basic code for squarify

--- src/lib/Image.php
+++ src/lib/Image.php
@@ -29,6 +29,34 @@ class Image
     $this->image->resizeImage($width,$height, \Imagick::FILTER_CATROM, .8, true);
   }
 
+  private function createBlurredBackground($image, $quantiy = 15)
+  {
+    /** @var $image \Imagick */
+
+    $image->resizeImage(600, 600, \Imagick::FILTER_LANCZOS, 1, true);
+
+    $biggerSide = max( $image->getImageWidth(), $image->getImageHeight() );
+    $smallerSide = min( $image->getImageHeight(), $image->getImageWidth() );
+
+    $background = clone $image;
+    $background->resizeImage($biggerSide * $smallerSide, $biggerSide, \Imagick::FILTER_LANCZOS, 1, true);
+    $background->blurImage(50, 25);
+
+    $posTop = ($biggerSide / 2) - ($image->getImageHeight() / 2 );
+    $posLeft = ( $background->getImageWidth() / 2 ) - ( $image->getImageWidth() / 2 );
+
+    $res = $background->compositeImage($image, \Imagick::COMPOSITE_OVER, $posLeft, $posTop);
+
+    $background->cropImage($biggerSide, $biggerSide * $smallerSide, ($smallerSide/2)+($biggerSide/2), 0 );
+
+    $this->image = $background;
+  }
+
+  public function squarifyBlur($size)
+  {
+    $this->createBlurredBackground($this->image);
+  }
+
   public function save($path = null)
   {
     $file = $path == null ? $this->file : File::newFile($path, true);
--- tests/lib/ImageTest.php
+++ tests/lib/ImageTest.php
@@ -3,10 +3,18 @@
 class ImageTest extends PHPUnit_Framework_TestCase
 {
 
+  /** @var mangeld\lib\Image */
+  public $image;
+
+  public function setUp()
+  {
+    $this->image = mangeld\lib\Image::fromFile(\mangeld\lib\filesystem\File::openFile('/vagrant/photo.png'));
+  }
+
   public function testImage()
   {
     $image = mangeld\lib\Image::fromFile(\mangeld\lib\filesystem\File::openFile('/vagrant/photo.png'));
-    $image->blur(15);
+    $image->blur(1);
     $image->save('/vagrant/photo_blur.png');
   }
 
@@ -15,6 +23,12 @@ class ImageTest extends PHPUnit_Framework_TestCase
     $image = mangeld\lib\Image::fromFile(\mangeld\lib\filesystem\File::openFile('/vagrant/photo.png'));
     $image->resize(400, 400);
     $image->save('/vagrant/photo_resize.png');
+  }
+
+  public function testSquarifyBlur()
+  {
+    $this->image->squarifyBlur(600);
+    $this->image->save('/vagrant/photo_squarify.png');
   }
 
 }
