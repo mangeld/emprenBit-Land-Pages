d8c6f3b1940bc09d2f60513500730cbe7c56a228
Miguel Ángel Durán González <contact@mangel.me>
2015-05-09 14:56:09 +0200

    more tests

--- scripts/mk_image_versions.php
+++ scripts/mk_image_versions.php
@@ -7,7 +7,7 @@ use \mangeld\exceptions\FileSystemException;
 if( !isset( $argv[1] ) && !isset($argv[2]) ) exit(1);
 
 ini_set("log_errors", 1);
-ini_set("error_log", "/var/log/mkImages.log");
+ini_set("error_log", "error.log");
 
 $source = $argv[1];
 $destination = $argv[2];
