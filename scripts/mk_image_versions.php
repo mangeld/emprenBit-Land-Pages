98821fea27163d53cf025f56a02c84d1b5f75cce
Miguel Ángel Durán González <contact@mangel.me>
2015-05-09 14:49:56 +0200

    Some changes to log

--- scripts/mk_image_versions.php
+++ scripts/mk_image_versions.php
@@ -2,17 +2,22 @@
 
 include_once '../vendor/autoload.php';
 use \mangeld\lib\filesystem\File;
+use \mangeld\exceptions\FileSystemException;
 
 if( !isset( $argv[1] ) && !isset($argv[2]) ) exit(1);
 
 ini_set("log_errors", 1);
 ini_set("error_log", "/var/log/mkImages.log");
 
-
 $source = $argv[1];
 $destination = $argv[2];
-$image = File::openFile($source);
-$image->makeImageVersions($destination);
-File::openFile($source)->delete();
+try
+{
+  $image = File::openFile($source);
+  $image->makeImageVersions($destination);
+  File::openFile($source)->delete();
+}
+catch (FileSystemException $e)
+{ error_log('Error opening image $source'); }
 
 exit(0);
