a8a4fbc920a864fee44a8d8fb8aea7a835f4e568
Miguel Ángel Durán González <contact@mangel.me>
2015-05-09 14:11:57 +0200

    Fixing  error log

--- scripts/mk_image_versions.php
+++ scripts/mk_image_versions.php
@@ -5,6 +5,10 @@ use \mangeld\lib\filesystem\File;
 
 if( !isset( $argv[1] ) && !isset($argv[2]) ) exit(1);
 
+ini_set("log_errors", 1);
+ini_set("error_log", "/var/log/mkImages.log");
+
+
 $source = $argv[1];
 $destination = $argv[2];
 $image = File::openFile($source);
