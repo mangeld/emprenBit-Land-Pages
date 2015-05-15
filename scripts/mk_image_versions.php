df6fc27e3342037b30351f86c19176cf333923c8
Miguel Ángel Durán González <contact@mangel.me>
2015-05-15 09:45:21 +0200

    Issue #3 done

--- scripts/mk_image_versions.php
+++ scripts/mk_image_versions.php
@@ -4,7 +4,11 @@ include_once '../vendor/autoload.php';
 use \mangeld\lib\filesystem\File;
 use \mangeld\exceptions\FileSystemException;
 
-if( !isset( $argv[1] ) && !isset($argv[2]) ) exit(1);
+if( !isset( $argv[1] ) && !isset($argv[2]) )
+{
+  \mangeld\lib\Logger::instance()->warning("Not enought args given to mk_image_versions");
+  exit(1);
+}
 
 $source = $argv[1];
 $destination = $argv[2];
@@ -15,6 +19,6 @@ try
   File::openFile($source)->delete();
 }
 catch (FileSystemException $e)
-{ \mangeld\lib\Logger::instance()->error("Error opening image $source"); }
+{ \mangeld\lib\Logger::instance()->error("Error opening image $source in mk_image_versions"); }
 
 exit(0);
